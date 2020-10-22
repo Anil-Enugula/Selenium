e6381723fb5152cc2235616476de8b617a96dd1d
Anil-Enugula <73212415+Anil-Enugula@users.noreply.github.com>
2020-10-22 09:30:50 +0530

    Add files via upload

---
+++ src/Senerio_Day1/scenario4_Q1_Customised_xpath.java
@@ -0,0 +1,56 @@
+package Scenerio_Day1;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class scenario4_Q1_Customised_xpath {
+	WebDriver driver;
+	String url="https://www.google.com";
+
+
+	@Before
+	public void setUp() throws Exception {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver.exe" );
+		driver=new ChromeDriver();
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.close();
+	}
+
+	@Test
+	public void test() throws InterruptedException {
+		
+		driver.findElement(By.xpath(".//*[@title='Search']")).sendKeys("clothes");
+		Thread.sleep(3000);
+		List<WebElement> list1=driver.findElements(By.xpath(".//*[@role='listbox']/li"));
+		for(WebElement web:list1)
+		{
+			if(web.getText().trim().equals("clothes dryer"))
+			{
+				web.click();
+				break;
+			}
+		}
+		Thread.sleep(5000);
+	
+	}
+
+}
